e88b8d9465df971efa9cfa0aa38a52e986953e2f
Ushard <robin.duman@hotmail.com>
2022-01-19 10:50:45 +0100

    Complétion User dans models.py

--- sql/models.py
+++ sql/models.py
@@ -19,9 +19,20 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(256), unique=True, index=True)
+    est_rh = Column(Boolean, nullable=False)
+    notation = Column(Float)
+    ba_id = relationship("BA") #TODO
+    prenom = Column(String(256))
+    nom_de_famille = Column(String(256))
+    campus = Column(String(256))
+    promo = Column(String(256))
+    etudes_realisees = relationship("Etudes") #TODO
+    etudes_postulees = relationship("Etudes") #TODO
 
     hashed_password = Column(String(256))
     account_creation_date = Column(DateTime, default=datetime.utcnow())
+    ba_est_complet = Column(Boolean, default=False) #TODO
+    est_premium = Column(Boolean, defautl=False) #TODO
 
     def verify_password(self, plain_password):
         password_is_correct = pwd_context.verify(plain_password, self.hashed_password)
@@ -31,3 +42,10 @@ class User(Base):
         self.hashed_password = get_password_hash(plain_password)
         return True
     
+    def marquer_ba_complet(self):
+        self.ba_est_complet = True
+        return
+
+    def mettre_premium(self):
+        self.ba_est_complet = True
+        return
