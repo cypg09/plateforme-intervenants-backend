7c4cc218b77e64abdf3e645baf1fe0a6cd54fec3
Ushard <robin.duman@hotmail.com>
2022-01-25 13:36:32 +0100

    get_etudes_non_archivees >> get_etudes ds crud

--- sql/crud.py
+++ sql/crud.py
@@ -71,13 +71,14 @@ def get_etude_by_id(db: Session, etude_id: int):
 def get_nombre_etudes(db: Session):
     return db.query(models.Etude).filter_by(est_archivee=False).count()
 
+
 @use_db
-def get_etudes_non_archivees(db: Session, page: int =1, cards_per_page: int =30):
-    etudes_non_archivees = db.query(models.Etude).filter_by(est_archivee=False).all()
+def get_etudes(db: Session, page: int =1, cards_per_page: int =30, est_archivee: bool =False):
+    etudes = db.query(models.Etude).filter_by(est_archivee=est_archivee).all()
     page = 1 if page < 1 else page
-    # borne inf: 0 si on est page 1, sinon 31, 61 etc
+    #borne inf: 0 so on est page 1, sinon 31, 61 etc
     borne_inf = 0 if page == 1 else 1 + (page - 1) * cards_per_page 
     # borne sup: 31, 61, ...
     borne_sup = 1 + page * cards_per_page
-    etudes_non_archivees_paginees = etudes_non_archivees[borne_inf:borne_sup]
-    return etudes_non_archivees_paginees
+    etudes_paginees = etudes[borne_inf:borne_sup]
+    return etudes_paginees
